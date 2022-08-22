5e33401837d0ad197f46b114d102926f1014cf27
DESKTOP-086N6D3\danma <danmachi1472@gmail.com>
2022-08-22 19:37:12 +0800

    math test

--- GitHubSample/src/gittest/helo.java
+++ GitHubSample/src/gittest/helo.java
@@ -3,5 +3,11 @@ package gittest;
 public class helo {
 	public static void main(String[] args) {
 		System.out.println("hello");
+		System.out.println("hello2");
+	}
+}
+class Math{
+	int add(int a, int b) {
+		return a + b;
 	}
 }
